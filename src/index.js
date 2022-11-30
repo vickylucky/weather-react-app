f96b62587422e1d188a607daf553fc450dd180d7
Victoria <victoria.belokon@gmail.com>
2022-12-01 00:23:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import ReactDom from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDom.render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.querySelector("#weather")
+  </React.StrictMode>
 );
+
+reportWebVitals();
+
